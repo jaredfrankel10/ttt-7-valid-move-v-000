8ce09cb5c0ea63e79b92d560527bd146ca75d770
jared frankel <jaredfrankel10@gmail.com>
2017-09-14 15:04:24 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,5 +1,5 @@
 def valid_move?(board, index)
-  if valid_move?(board, index) == "" || valid_move?(board, index) == " " || valid_move?(board, index) == nil
+  if board[index] == "" || board[index] == " " || board[index] == nil
     false
   elsif board[index] == "X" || board[index] == "O"
   true
